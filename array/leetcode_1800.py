9ae0733ed50505501f18282c14e280f6219001ee
devle <dltjgus0709@gmail.com>
2025-02-04 21:51:29 +0900

    1800. Maximum Ascending Subarray Sum

---
+++ array/leetcode_1800.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxAscendingSum(self, nums):
+        n = len(nums)
+        max_sum = nums[0]
+        curr_sum = nums[0]
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                curr_sum += nums[i]
+            else:
+                curr_sum = nums[i]
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
